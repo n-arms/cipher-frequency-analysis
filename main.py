58d068a30e65404a4d20827de619bfc0a94c4c07
narms <75282394+n-arms@users.noreply.github.com>
2021-02-09 18:48:44 -0500

    Read from example.txt and change format of data.json

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-from tkinter import *
+from queue import PriorityQueue
 import json
+
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+alphabet = {i for i in lower_case+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
+
+with open("data.json") as json_file:
+    with open("example.txt") as text:
+        data = json.load(json_file)
+        letters = {i: 0 for i in lower_case}
+        for line in text:
+            for char in line:
+                if char in alphabet:
+                    letters[char.lower()] += 1
+        print(letters)
+        
