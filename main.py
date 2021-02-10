a4c116540f89d7986ad73d4c39d37166a8fdbcab
narms <75282394+n-arms@users.noreply.github.com>
2021-02-09 20:08:34 -0500

    Add caeser function, implement decoding, write to example.txt
    * example.txt contains alice in wonderland shifted 42 digits over
    * decoding is relatively accurate

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
 from queue import PriorityQueue
 import json
 
-
 lower_case = "abcdefghijklmnopqrstuvwxyz"
 alphabet = {i for i in lower_case+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
+upper_case = {i for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
 
-with open("data.json") as json_file:
-    with open("example.txt") as text:
-        data = json.load(json_file)
-        letters = {i: 0 for i in lower_case}
+
+def caesar(file_name, shift):
+    encodings = {lower_case[i]: lower_case[(shift+i)%26] for i in range(26)}
+    
+    with open(file_name) as text:
         for line in text:
             for char in line:
                 if char in alphabet:
-                    letters[char.lower()] += 1
-        print(letters)
+                    if char in upper_case:
+                        print(encodings[char.lower()], end='')
+                    else:
+                        print(encodings[char], end='')
+                else:
+                    print(char, end='')
+
+with open("data.json") as json_file:
+    data = json.load(json_file)
+    
+with open("example.txt") as text:
+    letters = {i: 0 for i in lower_case}
+    for line in text:
+        for char in line:
+            if char in alphabet:
+                letters[char.lower()] += 1
+    ordering = PriorityQueue()
+    for i in letters:
+        ordering.put((-1*letters[i], i))
+    decoded = {}
+    i = 0
+    while not ordering.empty():
+        encoded = ordering.get()
+        decoded[encoded[1]] = data["frequency-letters"][i]
+        i += 1
+
+with open("example.txt") as text:
+    for line in text:
+        for char in line:
+            if char in alphabet:
+                print(decoded[char.lower()], end='')
+            else:
+                print(char, end='')
+
+
+
+    
+    
         
